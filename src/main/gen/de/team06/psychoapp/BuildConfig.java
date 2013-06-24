cbc05d92963bb0a24999fe8c7b6adf8999fd816a
Andi K <unecht@mail.com>
2013-06-24 20:21:33 +0200

    renamed layout files due to compiling reasons!

---
+++ src/main/gen/de/team06/psychoapp/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package de.team06.psychoapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
