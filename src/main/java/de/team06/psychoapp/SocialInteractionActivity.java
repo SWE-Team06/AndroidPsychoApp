ee8feb53cf1dcdc673e512f526d07510817c2ece
Andi K <unecht@mail.com>
2013-06-30 21:13:13 +0200

    - remove csv-exporter from manifest, sry the preliminary fix was failed - add SocialInteractionActivity

---
+++ src/main/java/de/team06/psychoapp/SocialInteractionActivity.java
@@ -0,0 +1,17 @@
+package de.team06.psychoapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 30.06.13.
+ */
+
+public class SocialInteractionActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input);
+    }
+}
