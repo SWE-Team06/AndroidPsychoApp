8135327f4a6a1186361be145c189f89d252b8c73
Gheek <kevin@keen-designs.de>
2013-07-04 15:22:38 +0200

    Andys Alarm Fix

--- src/main/java/de/team06/psychoapp/AlarmReceiver.java
+++ src/main/java/de/team06/psychoapp/AlarmReceiver.java
@@ -8,11 +8,19 @@ import android.content.Intent;
  * Created by Andi on 30.06.13.
  */
 
-public class AlarmReceiver extends BroadcastReceiver {
+public class AlarmReceiver extends BroadcastReceiver
+{
     @Override
-    public void onReceive(Context ourAppScope, Intent startSocialInteractionActivity) {
-        Intent start = new Intent(ourAppScope.getApplicationContext(), SocialInteractionActivity.class);
-        start.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  //damnd shit! I spent 2-3h of my time for this one line of code!!!
-        ourAppScope.getApplicationContext().startActivity(start);
+    public void onReceive(Context ourAppScope, Intent intentFromAlarmManager)
+    {
+        /*
+            creates an intent to start our "SocialInteractionActivity"
+            you have to add the "FLAG_ACTIVITY_NEW_TASK" flag, otherwise the app will be collapse!
+         */
+
+        Intent startSocialInteractionActivity = new Intent(ourAppScope, SocialInteractionActivity.class);
+        startSocialInteractionActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        ourAppScope.getApplicationContext().startActivity(startSocialInteractionActivity);
     }
 };
--- src/main/java/de/team06/psychoapp/SocialInteractionActivity.java
+++ src/main/java/de/team06/psychoapp/SocialInteractionActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
  * Created by Andi on 30.06.13.
  */
 
-public class SocialInteractionActivity extends Activity {
+public class SocialInteractionActivity extends Activity
+{
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input);
     }
